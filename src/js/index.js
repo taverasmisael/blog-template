ce2a37c75334f552f1d8676f6ae4f160719e2ce9
taverasmisael <macrot29@gmail.com>
2018-03-27 22:04:00 -0400

    instantiate navmenu and complete its functionality

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,12 @@
 import LoadPageScript from './loadPageScript'
+import NavMenu from './navmenu'
 
 const currentPage = document.location.pathname.split('.')[0]
 document.addEventListener('DOMContentLoaded', () => {
   LoadPageScript(currentPage).then(({ default: InitView }) => InitView())
+  NavMenu({
+    navSelector: '#MainNav',
+    navActiveClass: 'nav--active',
+    navTogglers: '.js--nav-toggler'
+  })
 })
