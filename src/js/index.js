45b1acb28f36c50cecd06e3f98eb9b892e944c28
taverasmisael <macrot29@gmail.com>
2018-03-30 18:46:51 -0400

    lazy load images

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,12 @@
+import LazyLoad from 'vanilla-lazyload'
 import LoadPageScript from './loadPageScript'
 import NavMenu from './navmenu'
 
 const currentPage = document.location.pathname.split('.')[0]
+const lazyLoad = new LazyLoad({
+  callback_load: console.log.bind(console, 'Loading...')
+})
+
 document.addEventListener('DOMContentLoaded', () => {
   LoadPageScript(currentPage).then(({ default: InitView }) => InitView())
   NavMenu({
@@ -9,4 +14,5 @@ document.addEventListener('DOMContentLoaded', () => {
     navActiveClass: 'nav--active',
     navTogglers: '.js--nav-toggler'
   })
+  console.log(lazyLoad)
 })
